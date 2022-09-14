b36730042351141d4434ee654bdce68f26b3fd5c
Ritesh <ritesh2897pandey@gmail.com>
2022-09-14 17:13:40 +0530

    created model folder add model init.

---
+++ post_module/models/__init__.py
@@ -0,0 +1,3 @@
+from .author import Author
+from .post  import Post
+from .tags  import Tag
---
+++ post_module/models/post.py
@@ -0,0 +1,22 @@
+from django.db import models
+from .tags import Tag
+from .author import Author
+
+class Post(models.Model):
+    
+    title = models.CharField(max_length=255, unique=True)
+    subtitle = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    body = models.TextField()
+    description = models.CharField(max_length=150, blank=True)
+    published_date = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    published = models.BooleanField(default=False)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, blank=True)
+
+    class Meta:
+        ordering = ["-published_date"]
+
+    def __str__(self):
+        return self.title
